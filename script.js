f9b61d1dfff5b43da6c4a3802bfdf39186dace66
Eyitayo Eniola <eyitayoeniola7@gmail.com>
2025-10-14 23:00:49 +0100

    Updated more work on responsiveness

--- script.js
+++ script.js
@@ -1,28 +1,2 @@
-document.addEventListener('DOMContentLoaded', function() {
-            const swiper = new Swiper('.swiper-container', {
-                slidesPerView: 1,
-                spaceBetween: 30,
-                loop: true,
-                speed: 800,
-                autoplay: {
-                    delay: 2000,
-                    disableOnInteraction: false,
-                },
-                navigation: {
-                    nextEl: '.swiper-button-next',
-                    prevEl: '.swiper-button-prev',
-                },
-                breakpoints: {
-                    768: {
-                        slidesPerView: 2,
-                        spaceBetween: 20,
-                    },
-                    1024: {
-                        slidesPerView: 3,
-                        spaceBetween: 30,
-                    }
-                }
-            });
-        });
 
         
